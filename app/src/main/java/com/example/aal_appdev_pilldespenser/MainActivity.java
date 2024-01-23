78e2af1faadbd6ecf198996671a9362488727977
code2impress <meno.dittrich@gmail.com>
2024-01-23 21:18:58 +0100

    Doc UI added
    Added V1 of Doc UI including sliders for each weekday

--- app/src/main/java/com/example/aal_appdev_pilldespenser/MainActivity.java
+++ app/src/main/java/com/example/aal_appdev_pilldespenser/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.aal_appdev_pilldespenser;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton button2 = findViewById(R.id.button2);
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, docgenqr.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/aal_appdev_pilldespenser/docgenqr.java
@@ -0,0 +1,14 @@
+package com.example.aal_appdev_pilldespenser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class docgenqr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_docgenqr);
+    }
+}
