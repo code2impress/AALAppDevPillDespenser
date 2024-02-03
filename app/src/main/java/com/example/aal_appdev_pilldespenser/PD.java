bf411431a410870e9a4bed18e731876399c25da3
code2impress <meno.dittrich@gmail.com>
2024-02-03 14:55:57 +0100

    Send mqtt once and once only
    Updated send button

--- app/src/main/java/com/example/aal_appdev_pilldespenser/PD.java
+++ app/src/main/java/com/example/aal_appdev_pilldespenser/PD.java
@@ -10,6 +10,8 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class PD extends AppCompatActivity {
 
+    private boolean isDataSent = false; // Flag to check if data has been sent
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,7 +22,11 @@ public class PD extends AppCompatActivity {
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                sendData(scannedData);
+                if (!isDataSent) { // Check if data hasn't been sent yet
+                    sendData(scannedData);
+                    imageButton.setEnabled(false); // Disable the button
+                    imageButton.setAlpha(0.4f); // Change button opacity to indicate it's disabled
+                }
             }
         });
     }
@@ -37,6 +43,7 @@ public class PD extends AppCompatActivity {
                         MqttMessage message = new MqttMessage(data.getBytes());
                         client.publish("aalpd2023", message);
                         client.disconnect();
+                        isDataSent = true; // Update flag after successful data send
                     } catch (MqttException e) {
                         e.printStackTrace();
                         // Handle error
